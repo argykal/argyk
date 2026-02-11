7eab1387a0cbfe5009e5aeea12c70aaefd5f92a9
argykaL <argyris.kalampoukas@gmail.com>
2026-02-11 10:56:06 +0200

    Personalize

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,22 +6,22 @@ import { Card } from "../components/card";
 
 const socials = [
 	{
-		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
-		label: "Twitter",
-		handle: "@chronark_",
+		icon: <Instagram size={20} />,
+		href: "https://instagram.com/argyris_kal",
+		label: "Instagram",
+		handle: "@argykal",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:argyris.kalampoukas@gmail.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "argyris.kalampoukas@gmail.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/argykal",
 		label: "Github",
-		handle: "chronark",
+		handle: "argykal",
 	},
 ];
 
